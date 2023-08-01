a6f40ee6a2dc7205a8126849c3968a2482f861b9
K1t1n <roman.anisim@gmail.com>
2023-08-01 19:01:23 +0300

    Остальное

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #include <Arduino.h>
+
 #include <EncButton.h>
+EncButton <EB_TICK, 2, 3, 4> enc;
+
 #include <ServoSmooth.h>
 
 
 
 void setup() {
-
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  enc.tick();
+  if (enc.turn()) Serial.println("turn");
+  if (enc.turnH()) Serial.println("hold+turn");
+  
 }
 
